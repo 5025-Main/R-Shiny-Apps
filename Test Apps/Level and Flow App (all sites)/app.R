7fc56102c1b7c5e9edcef13c106ab1b63bcd0519
gmcgurk <32911506+gmcgurk@users.noreply.github.com>
2019-07-10 13:17:07 -0700

    Lots of changes. See description
    changed file name to reflect updated app. CAR-072 Flow app -> Level and flow app all sites.
    
    Changed select input for paramters to checkboxes that allow for plotting multiple inputs on same plot.

---
+++ Test Apps/Level and Flow App (all sites)/app.R
@@ -0,0 +1,121 @@
+#packages go here
+library(shiny)
+library(tidyr)
+library(dplyr)
+library(ggplot2)
+theme_set(theme_minimal())
+
+# if we were using global variables, they would go here. This script is using user inputs from the Data/ file, so no need for global va
+
+
+options(shiny.reactlog=TRUE) 
+
+# User interface ----
+
+ui <- fluidPage(
+  selectInput('selectfile','Select File',choice = list.files('Data/')),
+  selectInput('selectfile2','Select File to compare',choice = list.files('Data/')),
+  
+  #selectInput("var", 
+             # label = "Choose a variable to display",
+              #choices = c("Level_in", "Level_in_clipped","Flow_gpm","Flow_gpm_nostormflow","Flow_gpm_USBR"),
+              #selected = "Level_in"),
+  
+  #selectInput("var2", 
+              #label = "Choose a secondary variable to display",
+              #choices = c( "None","Level_in", "Level_in_clipped","Flow_gpm","Flow_gpm_nostormflow","Flow_gpm_USBR"),
+             # selected = ""),
+
+  # input parameters
+  checkboxGroupInput("checkGroup", label = h3("Select Parameters"),
+               c("Level_in" = "Level_in",
+               "Level_in_clipped" = "Level_in_clipped",
+               "Flow_gpm"="Flow..gpm.",
+               "Flow_gpm_nostormflow" ="Flow..gpm..no.stormflow",
+               "Flow_gpm_USBR" ="Flow..gpm..USBR"), 
+               selected = "Level_in"),
+  
+  mainPanel(h4("Brush and double-click to select date range (Double-click anywhere to reset)"), plotOutput("plot",
+                       dblclick = "plot_dblclick",
+                       brush = brushOpts(
+                         id = "plot_brush",
+                         resetOnNew = TRUE)))
+  )
+
+
+
+# Server logic ----
+server <- function(input, output) {
+  
+  mydata <- reactive({
+ 
+  in.file <- input$selectfile
+  txt.str= paste('Data/',in.file,sep = "")
+  data= read.csv(txt.str)
+  #data=in.file
+  
+  data$X=as.character(data$X)
+  data$date.time=strptime(data$X, format="%Y-%m-%d %H:%M:%S") 
+  data$date.time = as.POSIXct(data$date.time)
+  return(data)
+  
+   })
+  
+  ranges <- reactiveValues(x = NULL, y = NULL)
+  
+  output$plot <- renderPlot({
+    data.m=mydata() 
+    #param <- switch(input$var, 
+                  #  "Level_in" = "Level_in",
+                  #  "Level_in_clipped" = "Level_in_clipped",
+                 #   "Flow_gpm"="Flow..gpm.",
+                 #  "Flow_gpm_nostormflow" ="Flow..gpm..no.stormflow",
+                 #  "Flow_gpm_USBR" ="Flow..gpm..USBR")
+   # param4 <- switch(input$checkGroup, 
+                   # "Level_in" = "Level_in",
+                   # "Level_in_clipped" =2,
+                    #"Flow_gpm"=3,
+                   #"Flow_gpm_nostormflow" =4,
+                   # "Flow_gpm_USBR" =5)
+    
+  #  param2= switch(input$var2, 
+                  # "Level_in" = "Level_in",
+                  # "Level_in_clipped" = "Level_in_clipped",
+                  # "Flow_gpm"="Flow..gpm.",
+                  # "Flow_gpm_nostormflow" ="Flow..gpm..no.stormflow",
+                  # "Flow_gpm_USBR" ="Flow..gpm..USBR")
+ 
+    
+    df2 <- data.m %>%
+      select(date.time, input$checkGroup) %>%
+      gather(key = "variable", value = "value", -date.time) 
+
+    
+    # Multiple line plot
+    ggplot(df2, aes(x = as.POSIXct(date.time), y = value)) + 
+      geom_line(aes(color = variable), size = 1) +
+     # scale_color_manual(values = c("#00AFBB", "#E7B800","#E7B800","#E7B800","#E7B800")) + 
+      labs(title=paste("Currently plotting",input$selectfile),
+           x ="Date", y = input$checkGroup)+
+      theme_minimal()+coord_cartesian(xlim = ranges$x, ylim = ranges$y, expand = FALSE)
+    
+    
+})
+
+  observeEvent(input$plot_dblclick, {
+    brush <- input$plot_brush
+    if (!is.null(brush)) {
+      ranges$x <- c(as.POSIXct(brush$xmin,origin = "1970-01-01"), as.POSIXct(brush$xmax,origin = "1970-01-01"))
+     
+      ranges$y <- c(brush$ymin, brush$ymax)
+      
+    } else {
+      ranges$x <- NULL
+      ranges$y <- NULL
+    }
+  })
+  
+}
+
+# Run app ----
+shinyApp(ui, server)
