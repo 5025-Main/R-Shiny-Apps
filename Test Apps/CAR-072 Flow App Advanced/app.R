e43127a2b187f1dda82616c466b580d410b5a4f7
gmcgurk <32911506+gmcgurk@users.noreply.github.com>
2019-07-10 09:01:00 -0700

    selectable file feature added
    app now allows the user to select a file from the shiny server for plotting

--- Test Apps/CAR-072 Flow App Advanced/app.R
+++ Test Apps/CAR-072 Flow App Advanced/app.R
@@ -1,5 +1,5 @@
 #packages go here
-
+library(shiny)
 library(tidyr)
 library(dplyr)
 library(ggplot2)
@@ -7,35 +7,39 @@ theme_set(theme_minimal())
 
 #global variables go here
 
-in.file= read.csv("Data/CAR-072-Flow.csv")
-data=in.file
 
-data$X=as.character(data$X)
+#in.file= read.csv("Data/CAR-072-Flow.csv")
+#data=in.file
+
+#data$X=as.character(data$X)
 
 #data$date.time=as.Date(data$X, format="%Y-%m-%d %H:%M:%S")   
-data$date.time=strptime(data$X, format="%Y-%m-%d %H:%M:%S") 
-data$date.time = as.POSIXct(data$date.time)
+#data$date.time=strptime(data$X, format="%Y-%m-%d %H:%M:%S") 
+#data$date.time = as.POSIXct(data$date.time)
 
-
+options(shiny.reactlog=TRUE) 
 
 # User interface ----
 
 ui <- fluidPage(
+  selectInput('selectfile','Select File',choice = list.files('Data/')),
+  textOutput('fileselected'),
   
   selectInput("var", 
               label = "Choose a variable to display",
               choices = c("Level_in", "Level_in_clipped","Flow_gpm","Flow_gpm_nostormflow","Flow_gpm_USBR"),
               selected = "Level_in"),
+  
   selectInput("var2", 
               label = "Choose a secondary variable to display",
               choices = c( "None","Level_in", "Level_in_clipped","Flow_gpm","Flow_gpm_nostormflow","Flow_gpm_USBR"),
               selected = ""),
 
-  dateRangeInput("daterange1", "Date range:",
-                 start = "2015-05-01",
-                 end   = "2018-9-30"),
+  #dateRangeInput("daterange1", "Date range:",
+               #  start = "2015-05-01",
+               #  end   = "2018-9-30"),
   
-  mainPanel(plotOutput("plot",
+  mainPanel(h4("Brush and double-click to select date range"), plotOutput("plot",
                        dblclick = "plot_dblclick",
                        brush = brushOpts(
                          id = "plot_brush",
@@ -46,9 +50,25 @@ ui <- fluidPage(
 
 # Server logic ----
 server <- function(input, output) {
+  
+  mydata <- reactive({
+ 
+  in.file <- input$selectfile
+  txt.str= paste('Data/',in.file,sep = "")
+  data= read.csv(txt.str)
+  #data=in.file
+  
+  data$X=as.character(data$X)
+  data$date.time=strptime(data$X, format="%Y-%m-%d %H:%M:%S") 
+  data$date.time = as.POSIXct(data$date.time)
+  return(data)
+  
+   })
+  
   ranges <- reactiveValues(x = NULL, y = NULL)
   
   output$plot <- renderPlot({
+    data.m=mydata() 
     param <- switch(input$var, 
                     "Level_in" = "Level_in",
                     "Level_in_clipped" = "Level_in_clipped",
@@ -63,7 +83,8 @@ server <- function(input, output) {
                    "Flow_gpm_nostormflow" ="Flow..gpm..no.stormflow",
                    "Flow_gpm_USBR" ="Flow..gpm..USBR")
   
-    df2 <- data %>%
+     
+    df2 <- data.m %>%
       select(date.time, param, param2) %>%
       gather(key = "variable", value = "value", -date.time)
    
@@ -72,7 +93,7 @@ server <- function(input, output) {
     ggplot(df2, aes(x = as.POSIXct(date.time), y = value)) + 
       geom_line(aes(color = variable), size = 1) +
       scale_color_manual(values = c("#00AFBB", "#E7B800")) + 
-      labs(title="Title",
+      labs(title=paste("Currently plotting",input$selectfile),
            x ="Date", y = param)+
       theme_minimal()+coord_cartesian(xlim = ranges$x, ylim = ranges$y, expand = FALSE)
     
